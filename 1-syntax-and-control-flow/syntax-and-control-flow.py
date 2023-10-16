ffd573032b896a0088c6e049b9bd83b67e5d62ee
Harrison Royds <harrisonroyds@yahoo.co.uk>
2023-10-16 18:46:37 +0100

    2.4 finished

---
+++ 1-syntax-and-control-flow/syntax-and-control-flow.py
@@ -0,0 +1,18 @@
+my_favourite_things = [
+    "raindrops on roses",
+    "whiskers on kittens",
+    "bright copper kettles",
+    "warm woolen mittens",
+]
+
+# for i in my_favourite_things:
+#     print(i)
+
+# A loop which prints the numbers 5 to 1 on separate lines
+
+for i in range(5, 0, -1):
+    print(i)
+print("THUNDERBIRDS ARE GO 🚀")
+
+for i in range(1, 11):
+    print(f"3 x {i} = {i * 3}")
